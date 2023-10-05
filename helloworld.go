4d17685581e3bfbc5eaf2d923fbed5054f7a99d4
Audacious Tux <tangimhossain1@gmail.com>
2023-10-05 10:13:32 +0000

    test

--- helloworld.go
+++ helloworld.go
@@ -11,9 +11,9 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	log.Print("helloworld: received a request")
 	target := os.Getenv("TARGET")
 	if target == "" {
-		target = "world"
+		target = "World"
 	}
-	fmt.Fprintf(w, "Hello %s!\n", target)
+	fmt.Fprintf(w, "Henlo %s!\n", target)
 }
 
 func main() {
